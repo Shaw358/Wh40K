878067307cfc56f7994a6b4fd63b849429c020ad
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-05-26 19:13:09 +0200

    Is gonna be one long ass development time

---
+++ Warhammer40K/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private int movementSpeed;
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back* Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * movementSpeed;
+        }
+    }
+}
