e891a3b6ed0849bddcf0b6378360c84c91ce1de1
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-05-26 18:55:42 +0200

    planet Update

---
+++ Warhammer40K/Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    private float rotation_speed;
+
+    private void Awake()
+    {
+        rotation_speed = 0.1f;    
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, rotation_speed, 0);
+    }
+}
