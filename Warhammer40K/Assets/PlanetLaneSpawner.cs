6331a1d1c707315594193216ef60fdbacb3d3ff8
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-09-03 15:16:54 +0200

    Fix

---
+++ Warhammer40K/Assets/PlanetLaneSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetLaneSpawner : MonoBehaviour
+{
+
+    TravelLanes[] travel_lanes;
+
+    private void Start()
+    {
+        travel_lanes = GetComponentsInChildren<TravelLanes>();
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        foreach(TravelLanes travel in travel_lanes)
+        {
+            travel.Setup();
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
+
--- Warhammer40K/Assets/TravelLanes.cs
+++ Warhammer40K/Assets/TravelLanes.cs
@@ -7,48 +7,81 @@ public class TravelLanes : MonoBehaviour
     List<LineRenderer> line_renderer = new List<LineRenderer>();
     private List<GameObject> accessible_planets = new List<GameObject>();
 
-    [SerializeField] private Color line_color;
+    List<GameObject> connected_planets = new List<GameObject>();
 
-    private void Start()
-    {
-        Setup();
-    }
+    [SerializeField] private Color line_color;
 
     public void Setup()
     {
         int radius = 15;
+
         Collider[] planets_in_range = Physics.OverlapSphere(transform.position, radius, 1 << 8);
 
-        for(int i = 0; i < planets_in_range.Length - 1; i++)
+        for(int j = 0; j < planets_in_range.Length; j++)
         {
-            if(planets_in_range[i].gameObject != gameObject)
-            accessible_planets.Add(planets_in_range[i].gameObject);
-            GameObject empty_gameobject = new GameObject();
+            accessible_planets.Add(planets_in_range[j].gameObject);
+        }
 
-            empty_gameobject.transform.parent = gameObject.transform;
-            
-            line_renderer.Add(empty_gameobject.AddComponent<LineRenderer>());
+        for (int i = 0; i < planets_in_range.Length - 1; i++)
+        {
+            if (!connected_planets.Contains(planets_in_range[i].gameObject))
+            {
+                GameObject empty_gameobject = new GameObject();
 
-            line_renderer[i].startWidth = 0.2f;
-            line_renderer[i].endWidth = 0.2f;
-            line_renderer[i].startColor = line_color;
-            line_renderer[i].endColor = line_color;
+                empty_gameobject.transform.parent = gameObject.transform;
 
-            Vector3 planet1 = transform.position;
-            planet1.y -= 1;
-            line_renderer[i].SetPosition(0, planet1);
+                line_renderer.Add(empty_gameobject.AddComponent<LineRenderer>());
 
-            Vector3 planet2 = accessible_planets[i].transform.position;
-            planet2.y -= 1;
-            line_renderer[i].SetPosition(1, planet2);
+                line_renderer[i].startWidth = 0.2f;
+                line_renderer[i].endWidth = 0.2f;
+                line_renderer[i].startColor = line_color;
+                line_renderer[i].endColor = line_color;
+
+                Vector3 planet1 = transform.position;
+                planet1.y -= 1;
+                line_renderer[i].SetPosition(0, planet1);
+
+                Vector3 planet2 = accessible_planets[i].transform.position;
+                planet2.y -= 1;
+                line_renderer[i].SetPosition(1, planet2);
+
+                accessible_planets[i].gameObject.transform.GetChild(0).GetComponent<TravelLanes>().SetConnectedPlanet(gameObject);
+
+
+
+                /*if(planets_in_range[i].gameObject != gameObject)
+                accessible_planets.Add(planets_in_range[i].gameObject);
+                GameObject empty_gameobject = new GameObject();
+
+                empty_gameobject.transform.parent = gameObject.transform;
+
+                line_renderer.Add(empty_gameobject.AddComponent<LineRenderer>());
+
+                line_renderer[i].startWidth = 0.2f;
+                line_renderer[i].endWidth = 0.2f;
+                line_renderer[i].startColor = line_color;
+                line_renderer[i].endColor = line_color;
+
+                Vector3 planet1 = transform.position;
+                planet1.y -= 1;
+                line_renderer[i].SetPosition(0, planet1);
+
+                Vector3 planet2 = accessible_planets[i].transform.position;
+                planet2.y -= 1;
+                line_renderer[i].SetPosition(1, planet2);*/
+            }
         }
     }
 
+    public void SetConnectedPlanet(GameObject planet)
+    {
+        connected_planets.Add(planet);
+    }
 
     private void OnDrawGizmos()
     {
         //int radius = 15;
         //Gizmos.DrawWireSphere(transform.position, radius);
     }
-    
+
 }
