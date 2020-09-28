5938a2700fd3291422ea8b4e7ae2d3ecaabdf933
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-09-28 20:20:45 +0200

    Finished V1.0 of info textbox

---
+++ Warhammer40K/Assets/Branch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Branch
+{
+
+    int power;
+
+    public void SetPower()
+    {
+
+    }
+
+    public void GetPower()
+    {
+
+    }
+}
--- Warhammer40K/Assets/Scripts/Debug stuff/FindMissingScripts.cs
+++ Warhammer40K/Assets/Scripts/Debug stuff/FindMissingScripts.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using UnityEditor;
-public class FindMissingScripts : EditorWindow
+public class FindMissingScripts// : EditorWindow
 {
-    [MenuItem("Window/FindMissingScripts")]
+    /*[MenuItem("Window/FindMissingScripts")]
     public static void ShowWindow()
     {
         EditorWindow.GetWindow(typeof(FindMissingScripts));
@@ -42,5 +42,5 @@ public class FindMissingScripts : EditorWindow
         }
 
         Debug.Log(string.Format("Searched {0} GameObjects, {1} components, found {2} missing", go_count, components_count, missing_count));
-    }
+    }*/
 }
